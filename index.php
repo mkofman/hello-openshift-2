2a54946923de1fea3aa98a41727756a74e17a67d
mkofman <40279962+mkofman@users.noreply.github.com>
2020-02-26 19:47:39 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+
+ <head>
+  <title>OCP Hello World</title>
+ </head>
+
+ <body>
+ <?php echo '<h2 style="color:#125BA8";>Hello OpenShift!</h2>'; ?> 
+ <?php $nextWeek = time() + (7 * 24 * 60 * 60);
+   echo 'Today is:       '. date('Y-m-d') ."\n"; ?>
+ </body>
+</html>
